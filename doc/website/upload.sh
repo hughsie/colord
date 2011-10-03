03fec53c0b9018bf3d6f22e15309e913a2a461be
Richard Hughes <richard@hughsie.com>
2011-10-03 10:37:40 +0100

    trivial: add a little upload script

---
+++ doc/website/upload.sh
@@ -0,0 +1,2 @@
+scp *.html *.css hughsient@annarchy.freedesktop.org:/srv/www.freedesktop.org/www/software/colord/
+scp ../api/html/* hughsient@annarchy.freedesktop.org:/srv/www.freedesktop.org/www/software/colord/gtk-doc/
