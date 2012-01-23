068bbad7062b92f419ea3472da7106a4184d67da
Richard Hughes <richard@hughsie.com>
2012-01-23 09:47:48 +0000

    Don't leak a GVariantIter when setting the device metadata

--- libcolord/cd-device.c
+++ libcolord/cd-device.c
@@ -464,7 +464,7 @@ cd_device_get_metadata_item (CdDevice *device, const gchar *key)
 static void
 cd_device_set_metadata_from_variant (CdDevice *device, GVariant *variant)
 {
-	GVariantIter *iter = NULL;
+	GVariantIter iter;
 	const gchar *prop_key;
 	const gchar *prop_value;
 
@@ -472,9 +472,8 @@ cd_device_set_metadata_from_variant (CdDevice *device, GVariant *variant)
 	g_hash_table_remove_all (device->priv->metadata);
 
 	/* insert the new metadata */
-	g_variant_get (variant, "a{ss}",
-		       &iter);
-	while (g_variant_iter_loop (iter, "{ss}",
+	g_variant_iter_init (&iter, variant);
+	while (g_variant_iter_loop (&iter, "{ss}",
 				    &prop_key, &prop_value)) {
 		g_hash_table_insert (device->priv->metadata,
 				     g_strdup (prop_key),
