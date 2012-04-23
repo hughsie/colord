d7e66ab7b365761eef4e044be9d4be50f2ff4606
Richard Hughes <richard@hughsie.com>
2012-04-23 12:38:30 +0100

    Add a GObject Introspection example

---
+++ examples/cd-find-device-by-property.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from gi.repository import Colord
+from gi.repository import Gio
+
+# connect to colord
+cancellable = Gio.Cancellable.new();
+client = Colord.Client.new()
+client.connect_sync(cancellable)
+
+# find the device with the correct parameters
+device = client.find_device_by_property_sync("XRANDR_name", "LVDS1", cancellable)
+device.connect_sync(cancellable)
+
+# get the default profile for the device
+profile = device.get_default_profile()
+profile.connect_sync(cancellable)
+
+print "default profile filename", profile.get_filename()
