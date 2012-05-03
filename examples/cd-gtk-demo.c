257d03d5e39f63ba7a2409ed1a3a3deecd6d5d91
Richard Hughes <richard@hughsie.com>
2012-05-03 10:27:41 +0100

    Add CdWindow which allows us to get the color profile for a GtkWidget
    This commit adds a gtk-3.0 dep to the new libcolord-gtk. You can split this out
    into a seporate subpackage if this makes things easier to install on headless
    things like print servers.

---
+++ examples/cd-gtk-demo.c
@@ -0,0 +1,86 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2011-2012 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+//gcc -o cd-gtk-demo cd-gtk-demo.c `pkg-config --cflags --libs colord-gtk` -Wall
+
+#include <stdlib.h>
+#include <colord-gtk.h>
+#include <gtk/gtk.h>
+
+static void
+cd_window_set_widget_cb (GObject *source,
+			 GAsyncResult *res,
+			 gpointer user_data)
+{
+	CdProfile *profile;
+	CdWindow *window = CD_WINDOW (source);
+	GError *error = NULL;
+
+	profile = cd_window_set_widget_finish (window,
+					       res,
+					       &error);
+	if (profile == NULL) {
+		g_warning ("failed to get output profile: %s", error->message);
+		g_error_free (error);
+		return;
+	}
+
+	g_debug ("screen profile to use %s", cd_profile_get_filename (profile));
+	g_object_unref (profile);
+}
+
+static void
+map_cb (GtkWidget *this_widget, gpointer user_data)
+{
+	CdWindow *window = CD_WINDOW (user_data);
+
+	/* get the profile for this widget */
+	cd_window_set_widget (window,
+			      this_widget,
+			      NULL,
+			      cd_window_set_widget_cb,
+			      this_widget);
+}
+
+int
+main (int argc, char *argv[])
+{
+	CdWindow *window;
+	gint retval = EXIT_FAILURE;
+	GtkWidget *dialog;
+
+	gtk_init (&argc, &argv);
+	window = cd_window_new ();
+	dialog = gtk_message_dialog_new (NULL,
+					 GTK_DIALOG_MODAL,
+					 GTK_MESSAGE_INFO,
+					 GTK_BUTTONS_OK,
+					 "%s", "Hello world");
+	g_signal_connect (dialog, "map",
+			  G_CALLBACK (map_cb),
+			  window);
+	gtk_dialog_run (GTK_DIALOG (dialog));
+	gtk_widget_destroy (dialog);
+	g_object_unref (window);
+
+	retval = EXIT_SUCCESS;
+	return retval;
+}
---
+++ libcolord-gtk/cd-self-test.c
@@ -0,0 +1,101 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2010 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "../libcolord/cd-profile.h"
+#include "cd-window.h"
+
+static void
+cd_window_set_widget_cb (GObject *source,
+			 GAsyncResult *res,
+			 gpointer user_data)
+{
+	CdWindow *window = CD_WINDOW (source);
+	GtkWidget *widget = GTK_WIDGET (user_data);
+	GError *error = NULL;
+	CdProfile *profile;
+
+	profile = cd_window_set_widget_finish (window,
+					       res,
+					       &error);
+	g_assert_no_error (error);
+	g_assert (profile != NULL);
+	g_assert (cd_profile_get_has_vcgt (profile));
+
+	g_debug ("profile was %s", cd_profile_get_filename (profile));
+	g_object_unref (profile);
+
+	/* kill the dialog */
+	gtk_widget_destroy (widget);
+}
+
+static void
+map_cb (GtkWidget *this_widget, gpointer user_data)
+{
+	CdWindow *window = CD_WINDOW (user_data);
+
+	/* get the profile for this widget */
+	cd_window_set_widget (window,
+			      this_widget,
+			      NULL,
+			      cd_window_set_widget_cb,
+			      this_widget);
+}
+
+static void
+colord_window_func (void)
+{
+	CdWindow *window;
+	GtkWidget *dialog;
+
+	window = cd_window_new ();
+	dialog = gtk_message_dialog_new (NULL,
+					 GTK_DIALOG_MODAL,
+					 GTK_MESSAGE_INFO,
+					 GTK_BUTTONS_OK,
+					 "%s", "Hello world");
+	g_signal_connect (dialog, "map",
+			  G_CALLBACK (map_cb),
+			  window);
+	gtk_dialog_run (GTK_DIALOG (dialog));
+	g_object_unref (window);
+}
+
+int
+main (int argc, char **argv)
+{
+	g_type_init ();
+	g_test_init (&argc, &argv, NULL);
+	gtk_init (&argc, &argv);
+
+	/* only critical and error are fatal */
+	g_log_set_fatal_mask (NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
+
+	/* tests go here */
+	g_test_add_func ("/colord/window", colord_window_func);
+	return g_test_run ();
+}
+
