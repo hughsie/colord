884dc6a1a5b41f73fee5bb76667dbd7321a13cdc
Richard Hughes <richard@hughsie.com>
2013-05-03 20:49:28 +0100

    trivial: Remove some debugging left in of cd-find-broken

--- client/cd-find-broken.c
+++ client/cd-find-broken.c
@@ -19,8 +19,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-//gcc -o cd-find-broken cd-find-broken.c `pkg-config --cflags --libs colord` -Wall
-
 #include <stdlib.h>
 #include <colord.h>
 #include <locale.h>
