4a0cf6c97d2fd5de5f74d2beaa485a550b805d5d
Richard Hughes <richard@hughsie.com>
2013-06-07 14:00:44 +0100

    trivial: Remove the website source
    This is only to be modified in master to avoid confusion.

--- doc/website/upload.sh
+++
@@ -1,2 +0,0 @@
-scp *.html *.css hughsient@annarchy.freedesktop.org:/srv/www.freedesktop.org/www/software/colord/
-scp ../api/html/* hughsient@annarchy.freedesktop.org:/srv/www.freedesktop.org/www/software/colord/gtk-doc/
