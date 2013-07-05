59d3f89cbbbbdcdc5cdad511e878b6aff2559e3b
Richard Hughes <richard@hughsie.com>
2013-07-05 11:40:30 +0100

    trivial: Require __COLORD_H_INSIDE__ for cd-icc-store.h

--- lib/colord/cd-icc-store.h
+++ lib/colord/cd-icc-store.h
@@ -19,6 +19,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
+#if !defined (__COLORD_H_INSIDE__) && !defined (CD_COMPILATION)
+#error "Only <colord.h> can be included directly."
+#endif
+
 #ifndef __CD_ICC_STORE_H
 #define __CD_ICC_STORE_H
 
