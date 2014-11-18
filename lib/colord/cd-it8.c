hughsie/colord

commit 0f0b5c89258b3781c0f73b2729e3665c31cdbd56

libcolord: Always replace CCSS spectral data of the same ID
When invoking cd_it8_load() multiple times on ccss files remove existing data
with the same ID.

lib/colord/cd-it8.c
@@ -697,7 +697,7 @@ cd_it8_load_ccss_spect (CdIt8 *it8, cmsHANDLE it8_lcms, GError **error)
 		cd_spectrum_set_start (spectrum, spectral_start);
 		cd_spectrum_set_end (spectrum, spectral_end);
 		cd_spectrum_set_norm (spectrum, spectral_norm);
-		g_ptr_array_add (it8->priv->array_spectra, spectrum);
+		cd_it8_add_spectrum (it8, spectrum);
 	}
 	return TRUE;
 }
@@ -801,7 +801,7 @@ cd_it8_load_cmf (CdIt8 *it8, cmsHANDLE it8_lcms, GError **error)
 		cd_spectrum_set_start (spectrum, spectral_start);
 		cd_spectrum_set_end (spectrum, spectral_end);
 		cd_spectrum_set_norm (spectrum, spectral_norm);
-		g_ptr_array_add (it8->priv->array_spectra, spectrum);
+		cd_it8_add_spectrum (it8, spectrum);
 	}
 	return TRUE;
 }

commit 9caf9ed103d2f5d9412bee8a6e02d94a5de7b0df

trivial: Fix memory leak introduced in 0f0b5c89258b3781c0f73b2729e3665c31cdbd56

lib/colord/cd-it8.c
@@ -697,6 +697,7 @@ cd_it8_load_ccss_spect (CdIt8 *it8, cmsHANDLE it8_lcms, GError **error)
 		cd_spectrum_set_end (spectrum, spectral_end);
 		cd_spectrum_set_norm (spectrum, spectral_norm);
 		cd_it8_add_spectrum (it8, spectrum);
+		cd_spectrum_free (spectrum);
 	}
 	return TRUE;
 }
@@ -801,6 +802,7 @@ cd_it8_load_cmf (CdIt8 *it8, cmsHANDLE it8_lcms, GError **error)
 		cd_spectrum_set_end (spectrum, spectral_end);
 		cd_spectrum_set_norm (spectrum, spectral_norm);
 		cd_it8_add_spectrum (it8, spectrum);
+		cd_spectrum_free (spectrum);
 	}
 	return TRUE;
 }