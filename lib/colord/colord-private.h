3e5a5b0decacc5146d9baafc6bba2ad91591afa8
Richard Hughes <richard@hughsie.com>
2015-10-13 13:30:44 +0100

    trivial: Add a missing file in a header for internal use only

--- lib/colord/colord-private.h
+++ lib/colord/colord-private.h
@@ -46,6 +46,7 @@
 #include <colord/cd-interp-linear.h>
 #include <colord/cd-interp.h>
 #include <colord/cd-it8.h>
+#include <colord/cd-it8-utils.h>
 #include <colord/cd-math.h>
 #include <colord/cd-quirk.h>
 #include <colord/cd-spectrum.h>
