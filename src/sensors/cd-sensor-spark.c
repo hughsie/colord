070e1584ead7b339b1d36e3e6221a0fa0ac08ae1
Richard Hughes <richard@hughsie.com>
2015-10-15 20:18:14 +0100

    Allow returning spectral readings from the Spark sensor

--- src/sensors/cd-sensor-spark.c
+++ src/sensors/cd-sensor-spark.c
@@ -36,6 +36,7 @@ typedef struct
 	GUsbDevice			*device;
 	GFile				*dark_cal_file;
 	CdSpectrum			*dark_cal;
+	CdSpectrum			*sensitivity_cal;
 } CdSensorSparkPrivate;
 
 static CdSensorSparkPrivate *
@@ -44,17 +45,154 @@ cd_sensor_spark_get_private (CdSensor *sensor)
 	return g_object_get_data (G_OBJECT (sensor), "priv");
 }
 
-static void
-_print_spectra (CdSpectrum *sp)
+static CdSpectrum *
+cd_sensor_spark_get_dark_calibration (CdSensor *sensor, GError **error)
 {
-	guint i, j;
-	gdouble scale = 180.f / cd_spectrum_get_value_max (sp);
-	for (i = 0; i < 1024; i+=5) {
-		g_print ("%.1fnm: ", cd_spectrum_get_wavelength (sp, i));
-		for (j = 0; j < cd_spectrum_get_value (sp, i) * scale; j++)
-			g_print ("*");
-		g_print ("\n");
+	CdSensorSparkPrivate *priv = cd_sensor_spark_get_private (sensor);
+	CdSpectrum *sp;
+	const gchar *kind;
+	g_autoptr(CdIt8) it8 = NULL;
+	g_autoptr(GError) error_local = NULL;
+
+	/* take a baseline (hopefully black) */
+	sp = osp_device_take_spectrum (priv->device, &error_local);
+	if (sp == NULL) {
+		g_set_error (error,
+			     CD_SENSOR_ERROR,
+			     CD_SENSOR_ERROR_NO_DATA,
+			     "failed to get spectrum: %s",
+			     error_local->message);
+		return NULL;
 	}
+
+	/* print something for debugging */
+	if (g_getenv ("SPARK_DEBUG") != NULL) {
+		g_autofree gchar *txt = NULL;
+		txt = cd_spectrum_to_string (sp, 180, 20);
+		g_print ("DARKCAL\n%s", txt);
+	}
+
+	/* save locally */
+	if (priv->dark_cal != NULL)
+		cd_spectrum_free (priv->dark_cal);
+	priv->dark_cal = cd_spectrum_dup (sp);
+	cd_spectrum_set_id (priv->dark_cal, "1");
+
+	/* save to file */
+	it8 = cd_it8_new ();
+	kind = cd_sensor_kind_to_string (cd_sensor_get_kind (sensor));
+	cd_it8_set_instrument (it8, kind);
+	cd_it8_set_kind (it8, CD_IT8_KIND_SPECT);
+	cd_it8_set_originator (it8, "colord");
+	cd_it8_set_normalized (it8, FALSE);
+	cd_it8_set_spectral (it8, TRUE);
+	cd_it8_set_enable_created (it8, TRUE);
+	cd_it8_set_title (it8, "Dark Calibration");
+	cd_it8_add_spectrum (it8, sp);
+	if (!cd_it8_save_to_file (it8,
+				  priv->dark_cal_file,
+				  &error_local)) {
+		g_set_error (error,
+			     CD_SENSOR_ERROR,
+			     CD_SENSOR_ERROR_INTERNAL,
+			     "failed to save dark calibration: %s",
+			     error_local->message);
+		return NULL;
+	}
+
+	return sp;
+}
+
+static CdSpectrum *
+cd_sensor_spark_get_spectrum (CdSensor *sensor,
+			      CdSensorCap cap,
+			      GError **error)
+{
+	CdSensorSparkPrivate *priv = cd_sensor_spark_get_private (sensor);
+	CdSpectrum *sp;
+	g_autoptr(CdSpectrum) sp_tmp = NULL;
+	g_autoptr(CdSpectrum) sp_biased = NULL;
+	g_autoptr(GError) error_local = NULL;
+
+	/* measure */
+	cd_sensor_set_state_in_idle (sensor, CD_SENSOR_STATE_MEASURING);
+
+	/* perform dark calibration */
+	if (cap == CD_SENSOR_CAP_CALIBRATION)
+		return cd_sensor_spark_get_dark_calibration (sensor, error);
+
+	/* we have no dark calibration */
+	if (priv->dark_cal == NULL ||
+	    cd_spectrum_get_size (priv->dark_cal) == 0) {
+		g_set_error_literal (error,
+				     CD_SENSOR_ERROR,
+				     CD_SENSOR_ERROR_REQUIRED_DARK_CALIBRATION,
+				     "no dark calibration provided");
+		return NULL;
+	}
+
+	/* get the spectrum */
+	sp_tmp = osp_device_take_spectrum (priv->device, &error_local);
+	if (sp_tmp == NULL) {
+		g_set_error (error,
+			     CD_SENSOR_ERROR,
+			     CD_SENSOR_ERROR_NO_DATA,
+			     "failed to get spectrum: %s",
+			     error_local->message);
+		return NULL;
+	}
+
+	/* print something for debugging */
+	if (g_getenv ("SPARK_DEBUG") != NULL) {
+		g_autofree gchar *txt = NULL;
+		txt = cd_spectrum_to_string (sp_tmp, 180, 20);
+		g_print ("RAW\n%s", txt);
+	}
+
+	/* we have an invalid dark calibration */
+	if (cd_spectrum_get_size (sp_tmp) !=
+	    cd_spectrum_get_size (priv->dark_cal)) {
+		g_set_error_literal (error,
+				     CD_SENSOR_ERROR,
+				     CD_SENSOR_ERROR_REQUIRED_DARK_CALIBRATION,
+				     "dark calibration was invalid");
+		return NULL;
+	}
+
+	/* print something for debugging */
+	if (g_getenv ("SPARK_DEBUG") != NULL) {
+		g_autofree gchar *txt = NULL;
+		txt = cd_spectrum_to_string (priv->dark_cal, 180, 20);
+		g_print ("DARKCAL\n%s", txt);
+	}
+
+	/* subtract the dark calibration */
+	sp_biased = cd_spectrum_subtract (sp_tmp, priv->dark_cal);
+	if (sp_biased == NULL) {
+		g_set_error_literal (error,
+				     CD_SENSOR_ERROR,
+				     CD_SENSOR_ERROR_NO_DATA,
+				     "failed to get subtract spectra");
+		return NULL;
+	}
+
+	/* print something for debugging */
+	if (g_getenv ("SPARK_DEBUG") != NULL) {
+		g_autofree gchar *txt = NULL;
+		txt = cd_spectrum_to_string (sp_biased, 180, 20);
+		g_print ("BIASED\n%s", txt);
+	}
+
+	/* multiply the spectrum with the correction curve */
+	sp = cd_spectrum_multiply (sp_biased, priv->sensitivity_cal, 1);
+
+	/* print something for debugging */
+	if (g_getenv ("SPARK_DEBUG") != NULL) {
+		g_autofree gchar *txt = NULL;
+		txt = cd_spectrum_to_string (sp, 180, 20);
+		g_print ("POSTMULT\n%s", txt);
+	}
+	return sp;
 }
 
 static void
@@ -64,72 +202,33 @@ cd_sensor_spark_sample_thread_cb (GTask *task,
 				  GCancellable *cancellable)
 {
 	CdSensor *sensor = CD_SENSOR (source_object);
-	CdSensorSparkPrivate *priv = cd_sensor_spark_get_private (sensor);
 	CdSensorCap cap = GPOINTER_TO_UINT (task_data);
 	CdSpectrum *illuminant = NULL;
 	g_autoptr(CdColorXYZ) sample = NULL;
 	g_autoptr(CdIt8) it8_cmf = NULL;
 	g_autoptr(CdIt8) it8_d65 = NULL;
-	g_autoptr(CdSpectrum) sp_new = NULL;
 	g_autoptr(CdSpectrum) sp = NULL;
 	g_autoptr(GError) error = NULL;
 	g_autoptr(GFile) file_cmf = NULL;
 	g_autoptr(GFile) file_illuminant = NULL;
 
-	/* measure */
-	cd_sensor_set_state_in_idle (sensor, CD_SENSOR_STATE_MEASURING);
-
-	/* perform dark calibration */
-	if (cap == CD_SENSOR_CAP_CALIBRATION) {
-		sp = osp_device_take_spectrum (priv->device, &error);
-		if (sp == NULL) {
-			g_task_return_new_error (task,
-						 CD_SENSOR_ERROR,
-						 CD_SENSOR_ERROR_NO_DATA,
-						 "%s", error->message);
-			return;
-		}
-		_print_spectra (sp);
-		if (priv->dark_cal != NULL)
-			cd_spectrum_free (priv->dark_cal);
-		priv->dark_cal = cd_spectrum_dup (sp);
-		cd_spectrum_set_id (priv->dark_cal, "1");
-
-		/* save to file */
-		it8_cmf = cd_it8_new ();
-		if (!cd_it8_save_to_file (it8_cmf,
-					  priv->dark_cal_file,
-					  &error)) {
-			g_task_return_new_error (task,
-						 CD_SENSOR_ERROR,
-						 CD_SENSOR_ERROR_INTERNAL,
-						 "%s", error->message);
-			return;
-		}
-
-		/* success */
-		sample = cd_color_xyz_new ();
-		g_task_return_pointer (task, sample, NULL);
-		sample = NULL;
-		return;
-	}
-
-	/* we have no dark calibration */
-	if (priv->dark_cal == NULL) {
+	/* get the correct spectra */
+	sp = cd_sensor_spark_get_spectrum (sensor, cap, &error);
+	if (sp == NULL) {
 		g_task_return_new_error (task,
-					 CD_SENSOR_ERROR,
-					 CD_SENSOR_ERROR_REQUIRED_DARK_CALIBRATION,
-					 "no dark calibration provided");
+					 error->domain,
+					 error->code,
+					 "%s", error->message);
 		return;
 	}
 
-	/* get the color */
-	sp = osp_device_take_spectrum (priv->device, &error);
-	_print_spectra (sp);
-
-	/* subtract the dark calibration */
-	sp_new = cd_spectrum_subtract (sp, priv->dark_cal);
-	_print_spectra (sp_new);
+	/* return dummy data */
+	if (cap == CD_SENSOR_CAP_CALIBRATION) {
+		CdColorXYZ *sample_tmp = NULL;
+		sample_tmp = cd_color_xyz_new ();
+		g_task_return_pointer (task, sample_tmp, NULL);
+		return;
+	}
 
 	/* load CIE1931 */
 	it8_cmf = cd_it8_new ();
@@ -158,7 +257,7 @@ cd_sensor_spark_sample_thread_cb (GTask *task,
 	/* convert to XYZ */
 	sample = cd_color_xyz_new ();
 	if (!cd_it8_utils_calculate_xyz_from_cmf (it8_cmf, illuminant,
-						  sp_new, sample,
+						  sp, sample,
 						  1.f, &error)) {
 		g_task_return_new_error (task,
 					 CD_SENSOR_ERROR,
@@ -188,6 +287,47 @@ cd_sensor_get_sample_async (CdSensor *sensor,
 
 CdColorXYZ *
 cd_sensor_get_sample_finish (CdSensor *sensor, GAsyncResult *res, GError **error)
+{
+	g_return_val_if_fail (g_task_is_valid (res, sensor), FALSE);
+	return g_task_propagate_pointer (G_TASK (res), error);
+}
+
+static void
+cd_sensor_spark_spectrum_thread_cb (GTask *task,
+				    gpointer source_object,
+				    gpointer task_data,
+				    GCancellable *cancellable)
+{
+	CdSensor *sensor = CD_SENSOR (source_object);
+	CdSensorCap cap = GPOINTER_TO_UINT (task_data);
+	CdSpectrum *spectrum;
+	GError *error = NULL;
+
+	/* success */
+	spectrum = cd_sensor_spark_get_spectrum (sensor, cap, &error);
+	if (spectrum == NULL) {
+		g_task_return_error (task, error);
+		return;
+	}
+	g_task_return_pointer (task, spectrum, NULL);
+}
+
+void
+cd_sensor_get_spectrum_async (CdSensor *sensor,
+			    CdSensorCap cap,
+			    GCancellable *cancellable,
+			    GAsyncReadyCallback callback,
+			    gpointer user_data)
+{
+	g_autoptr(GTask) task = NULL;
+	g_return_if_fail (CD_IS_SENSOR (sensor));
+	task = g_task_new (sensor, cancellable, callback, user_data);
+	g_task_set_task_data (task, GUINT_TO_POINTER (cap), NULL);
+	g_task_run_in_thread (task, cd_sensor_spark_spectrum_thread_cb);
+}
+
+CdSpectrum *
+cd_sensor_get_spectrum_finish (CdSensor *sensor, GAsyncResult *res, GError **error)
 {
 	g_return_val_if_fail (g_task_is_valid (res, sensor), FALSE);
 	return g_task_propagate_pointer (G_TASK (res), error);
@@ -249,8 +389,23 @@ cd_sensor_spark_lock_thread_cb (GTask *task,
 						 "%s", error->message);
 			return;
 		}
-		priv->dark_cal = cd_it8_get_spectrum_by_id (it8, "1");
+		priv->dark_cal = cd_spectrum_dup (cd_it8_get_spectrum_by_id (it8, "1"));
+		g_debug ("loaded dark calibration with %i elements",
+			 cd_spectrum_get_size (priv->dark_cal));
+
+		/* print something for debugging */
+		if (g_getenv ("SPARK_DEBUG") != NULL) {
+			g_autofree gchar *txt = NULL;
+			txt = cd_spectrum_to_string (priv->dark_cal, 180, 20);
+			g_print ("DARKCAL:\n%s", txt);
+		}
 	}
+
+	/* load the sensor sensitivity from a file */
+	priv->sensitivity_cal = cd_spectrum_new ();
+	cd_spectrum_set_start (priv->sensitivity_cal, 0);
+	cd_spectrum_set_end (priv->sensitivity_cal, 1000);
+	cd_spectrum_add_value (priv->sensitivity_cal, 150); // <- FIXME: this needs to come from the device itself
 
 	/* success */
 	g_task_return_boolean (task, TRUE);
@@ -297,6 +452,17 @@ cd_sensor_unlock_thread_cb (GTask *task,
 			return;
 		}
 		g_clear_object (&priv->device);
+	}
+
+	/* clear up the stuff we allocated in Lock() */
+	g_clear_object (&priv->dark_cal_file);
+	if (priv->sensitivity_cal != NULL) {
+		cd_spectrum_free (priv->sensitivity_cal);
+		priv->sensitivity_cal = NULL;
+	}
+	if (priv->dark_cal != NULL) {
+		cd_spectrum_free (priv->dark_cal);
+		priv->dark_cal = NULL;
 	}
 
 	/* success */
