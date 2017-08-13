f26b33cc5f2b7a89aea1bbd8c7a066e786b099d3
Richard Hughes <richard@hughsie.com>
2017-08-13 14:06:58 +0100

    trivial: Enable Vala in CI and the Fedora package

--- contrib/ci/build_and_test.sh
+++ contrib/ci/build_and_test.sh
@@ -4,7 +4,7 @@ export LC_ALL=C.UTF-8
 mkdir -p build && cd build
 rm * -rf
 meson .. \
-    -Denable-vala=false \
+    -Denable-vala=true \
     -Denable-print-profiles=false \
     -Denable-argyllcms-sensor=false \
     -Denable-sane=true \
